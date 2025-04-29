1b5c6f195d2effe52d01a86c9d7833c4a0977921
Lin24 <lindailydiaba@gmail.com>
2025-04-29 17:35:00 +0100

    add page

--- app/Filament/Resources/GeneratorResource/Pages/ListGenerators.php
+++ app/Filament/Resources/GeneratorResource/Pages/ListGenerators.php
@@ -4,7 +4,10 @@ namespace App\Filament\Resources\GeneratorResource\Pages;
 
 use App\Enum\GeneratorStatus;
 use App\Filament\Resources\GeneratorResource;
+use App\Models\Generator;
 use Filament\Actions;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\ViewField;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Resources\Pages\ListRecords\Tab;
 
@@ -13,7 +16,13 @@ class ListGenerators extends ListRecords
     protected static string $resource = GeneratorResource::class;
 
     protected static ?string $title = 'Groupes électrogènes';
+    public ?string $category = null;
+    public array $products;
 
+    public function updatedCategory($state)
+    {
+        $this->products = Generator::select('*')->get()->toArray();
+    }
     public function getTabs(): array
     {
         return  [
@@ -42,7 +51,35 @@ class ListGenerators extends ListRecords
                 ->icon('heroicon-o-plus'),*/
             Actions\Action::make('synchronuis')
                 ->label('Synchroniser')
-                ->icon('heroicon-o-arrow-path'),
+                ->icon('heroicon-o-arrow-path')
+                ->modalHeading("Synchroniser les produits")
+                ->modal()
+                ->form([
+                    Select::make('category')
+                        ->label('Sélectionnez la catégorie')
+                        ->searchable()
+                        ->preload()
+                        ->reactive()
+                        ->options([
+                            '1' => 'Categorie 1',
+                            '2' => 'Categorie 2',
+                            '3' => 'Categorie 3',
+                            '4' => 'Categorie 4'
+                        ])
+                        ->multiple()
+                        ->afterStateUpdated(function ($state) {
+                            $this->updatedCategory($state);
+                        }),
+
+                    $this->getProductViewField(),
+                ])
+                ->modalSubmitActionLabel('Synchroniser')
         ];
+    }
+
+    public function getProductViewField(): ViewField
+    {
+        return ViewField::make('product_table')
+            ->view('filament.generators.product-table');
     }
 }
